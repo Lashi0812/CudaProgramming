298eeb7edb14515fbfaa005cee6ee3edb4b73dbb
Lashi <lashi0812@gmail.com>
2023-09-26 17:55:09 +0000

    Trying CUB library

--- csrc/smemSquare.cu
+++ csrc/smemSquare.cu
@@ -2,6 +2,7 @@
 #include <cuda_runtime.h>
 #include <ATen/ATen.h>
 #include <iostream>
+#include <cub/cub.cuh>
 
 #define BDIMX 32
 #define BDIMY 32
@@ -23,6 +24,59 @@ __global__ void setRowReadRow(int *out)
     // load from shared mem and store it in global
     out[tid] = tile[threadIdx.y][threadIdx.x];
 }
+
+__global__ void setRowReadRowUsingCUB(int *out)
+{
+    typedef cub::BlockStore<int,BDIMX,1,cub::BLOCK_STORE_DIRECT,BDIMY,1,890> BlockStore;
+    __shared__ typename BlockStore::TempStorage temp_store;
+    // static allocate shared mem
+    __shared__ int tile[BDIMY][BDIMX];
+
+    // get the global thread id
+    int tid[1];
+    tid[0] = threadIdx.y * blockDim.x + threadIdx.x;
+
+    // store operation for shared mem
+    tile[threadIdx.y][threadIdx.x] = tid[0];
+
+    __syncthreads();
+    BlockStore(temp_store).Store(out,tid);
+}
+
+__global__ void setRowReadRowWithInput(int *in,int *out)
+{
+    // static allocate shared mem
+    __shared__ int tile[BDIMY][BDIMX];
+
+    // get the global thread id
+    int tid = threadIdx.y * blockDim.x + threadIdx.x;
+
+    // store operation for shared mem
+    tile[threadIdx.y][threadIdx.x] = in[tid] * 10;
+
+    __syncthreads();
+
+    // load from shared mem and store it in global
+    out[tid] = tile[threadIdx.y][threadIdx.x];
+}
+
+__global__ void setRowReadRowWithInputUsingCUB(int *in,int *out)
+{
+    typedef cub::BlockLoad<int,BDIMX,1,cub::BLOCK_LOAD_DIRECT,BDIMY,1,890> BlockLoad;
+    typedef cub::BlockStore<int,BDIMX,1,cub::BLOCK_STORE_DIRECT,BDIMY,1,890> BlockStore;
+
+    __shared__ typename BlockLoad::TempStorage temp_load;
+    __shared__ typename BlockStore::TempStorage temp_store;
+
+    int tid[1];
+
+    BlockLoad(temp_load).Load(in,tid); 
+    tid[0] *= 10;
+
+    __syncthreads();
+    BlockStore(temp_store).Store(out,tid);
+}
+
 
 __global__ void setColReadCol(int *out)
 {
@@ -128,11 +182,15 @@ int main(int argc, char *argv[])
         iprint = std::stoi(argv[1]);
 
     // host side
+    at::Tensor h_A = at::arange(nx*ny, at::kInt);
     at::Tensor gpuRef = at::zeros({nx, ny}, at::kInt);
 
     // device side
-    int *d_C;
+    int *d_A,*d_C;
+    CHECK_ERROR(cudaMalloc((int **)&d_A, nBytes));
     CHECK_ERROR(cudaMalloc((int **)&d_C, nBytes));
+
+    CHECK_ERROR(cudaMemcpy(d_A,h_A.data_ptr(),nBytes,cudaMemcpyHostToDevice));
 
     // execution configuration
     dim3 block(nx, ny);
@@ -143,6 +201,27 @@ int main(int argc, char *argv[])
     // launch kernel : setRowReadRow
     CHECK_ERROR(cudaMemset(d_C, 0, nBytes));
     setRowReadRow<<<grid, block>>>(d_C);
+    CHECK_ERROR(cudaMemcpy(gpuRef.data_ptr(), d_C, nBytes, cudaMemcpyDeviceToHost));
+    if (iprint)
+        std::cout << gpuRef << std::endl;
+    
+    // launch kernel : setRowReadRowUsingCUB
+    CHECK_ERROR(cudaMemset(d_C, 0, nBytes));
+    setRowReadRowUsingCUB<<<grid, block>>>(d_C);
+    CHECK_ERROR(cudaMemcpy(gpuRef.data_ptr(), d_C, nBytes, cudaMemcpyDeviceToHost));
+    if (iprint)
+        std::cout << gpuRef << std::endl;
+
+    // launch kernel : setRowReadRowWithInput
+    CHECK_ERROR(cudaMemset(d_C, 0, nBytes));
+    setRowReadRowWithInput<<<grid, block>>>(d_A,d_C);
+    CHECK_ERROR(cudaMemcpy(gpuRef.data_ptr(), d_C, nBytes, cudaMemcpyDeviceToHost));
+    if (iprint)
+        std::cout << gpuRef << std::endl;
+    
+    // launch kernel : setRowReadRowWithInputUsingCUB
+    CHECK_ERROR(cudaMemset(d_C, 0, nBytes));
+    setRowReadRowWithInputUsingCUB<<<grid, block>>>(d_A,d_C);
     CHECK_ERROR(cudaMemcpy(gpuRef.data_ptr(), d_C, nBytes, cudaMemcpyDeviceToHost));
     if (iprint)
         std::cout << gpuRef << std::endl;
@@ -182,6 +261,7 @@ int main(int argc, char *argv[])
     if (iprint)
         std::cout << gpuRef << std::endl;
 
+    CHECK_ERROR(cudaFree(d_A));
     CHECK_ERROR(cudaFree(d_C));
     CHECK_ERROR(cudaDeviceReset());
     return EXIT_SUCCESS;
